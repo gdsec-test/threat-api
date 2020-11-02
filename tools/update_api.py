ed1910fb87b710cea61a99d5f0c1f1c0c327f9f9
Prashanthi Kanniappan Murthy <pkanniappanmurt@godaddy.com>
2020-11-02 12:28:47 -0800

    Swagger combine initial commit

--- tools/update_api.py
+++ tools/update_api.py
@@ -6,6 +6,9 @@ the various API lambdas.
 """
 
 import boto3
+import json 
+import os
+import logging
 
 # NOTE: this uses hard-coded files for now; eventually this will be dynamically
 # generated and wired into CICD
@@ -18,7 +21,28 @@ def generate_swagger():
     present in the source tree.
     """
 
-    return open("../swagger.json").read()
+    parent_swagger = json.load(open("../swagger.json"))
+
+    for subdir, dirs, files in os.walk("../apis/"):
+        for filename in files:
+            filepath = subdir + os.sep + filename
+
+            if filepath.endswith("swagger.json"):
+                inside_dict = json.load(open(filepath))
+
+                for key,value in inside_dict["paths"].items():
+                    if key in parent_swagger["paths"].keys():
+                        logging.error(key+' already exists')
+                    parent_swagger["paths"][key]=value
+
+    with open("../swagger.json", "w") as fp:
+        json.dump(parent_swagger, fp, indent=2)
+    
+    return {'bytes': open("../swagger.json").read(), 'dictionary':parent_swagger}
+
+    # # reurn the dictionary ?
+    # return json.dumps(parent_swagger)
+
 
 
 def upload_swagger_json(swagger_spec):
@@ -33,7 +57,7 @@ def upload_swagger_json(swagger_spec):
         bucket_list[0].put_object(Body=swagger_spec, Key="swagger.json")
 
 
-def generate_api_definitions():
+def generate_api_definitions(parent_swagger):
     """\
     Build an API definition by consolidating information for the various
     lambdas.  For now, just return a hardcoded file present in the source tree,
@@ -44,7 +68,30 @@ def generate_api_definitions():
     sts = boto3.client("sts")
     aws_account_id = sts.get_caller_identity()["Account"]
 
-    return open("../api.json").read().replace("___AWS_ACCOUNT___", aws_account_id)
+    apigateway_integration =  {
+        "type": "aws_proxy",
+        "uri": "", # to be set later according to the funtions
+        "responses": {
+        "default": {
+            "statusCode": "200"
+        }
+        },
+        "passthroughBehavior": "when_no_match",
+        "httpMethod": "POST",
+        "contentHandling": "CONVERT_TO_TEXT"
+    }
+
+    for key in parent_swagger["paths"]:
+        current_dir = key.split(os.sep)[1]
+        uri = "arn:aws:apigateway:us-west-2:lambda:path/2015-03-31/functions/arn:aws:lambda:us-west-2:"+aws_account_id+":function:"+current_dir+"/invocations"
+        temp = apigateway_integration.copy()
+        temp["uri"]=uri
+        parent_swagger["paths"][key]["x-amazon-apigateway-integration"]=temp
+
+    with open("../api.json", "w") as fp:
+        json.dump(parent_swagger, fp, indent=2)
+
+    return open("../api.json").read()
 
 
 def update_apigateway(api_spec):
@@ -85,9 +132,9 @@ def update_apigateway(api_spec):
 
 if __name__ == "__main__":
     # Update swagger.json for SwaggerUI
-    swagger_json = generate_swagger()
-    upload_swagger_json(swagger_json)
+    swagger = generate_swagger() # returns [bytes, dictionary]
+    # upload_swagger_json(swagger['bytes']) # bytes used in uploading to S3
 
-    # Update the API Gateway specification
-    api_definitions = generate_api_definitions()
-    update_apigateway(api_definitions)
+    # # Update the API Gateway specification
+    api_definitions = generate_api_definitions(swagger['dictionary']) # dictionary used in api definition 
+    # update_apigateway(api_definitions)
