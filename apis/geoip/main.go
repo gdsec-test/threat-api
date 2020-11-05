11521df7fc1655db3b39be6781438824366f1bc8
Connor Lake <clake1@godaddy.com>
2020-11-05 13:06:59 -0800

    Added more tracing

--- apis/geoip/main.go
+++ apis/geoip/main.go
@@ -3,86 +3,115 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"fmt"
-	"log"
 	"net"
 	"net/http"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/oschwald/geoip2-golang"
+	"github.com/sirupsen/logrus"
 	"github.secureserver.net/threat/util/lambda/toolbox"
 
 	// This line adds apm tracing to this lambda
 	// Yep, it's that simple!
 	// Note that you must have the `ELASTIC_APM_SERVER_URL` and `ELASTIC_APM_API_KEY` env vars set
+	"go.elastic.co/apm"
 	_ "go.elastic.co/apm/module/apmlambda"
 )
 
 func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	b, err := json.Marshal(request)
+	// Get the toolbox
+	// This helps standardize things accross services
+	t := toolbox.GetToolbox()
+	httpClient := t.GetHTTPClient(nil)
+	// Defer sending of tracing info
+	defer func() {
+		t.Tracer.Flush(nil)
+		t.Tracer.SendMetrics(nil)
+		t.Tracer.Close()
+	}()
+
+	t.Logger.Info("Starting handling of request")
+
+	// Start transaction to process geoip request
+	geoipTx := t.Tracer.StartTransaction("GeoIP", "job")
+	defer geoipTx.End()
+	ctx = apm.ContextWithTransaction(ctx, geoipTx)
+
+	// Make a sample HTTP request
+	// The request should be traced
+	req, err := http.NewRequestWithContext(ctx, "GET", os.Getenv("ELASTIC_APM_SERVER_URL"), nil)
 	if err != nil {
-		fmt.Println("error:", err)
-		return events.APIGatewayProxyResponse{
-			StatusCode: http.StatusInternalServerError,
-			Body:       err.Error(),
-		}, err
+		// Capture error with tracer
+		apm.CaptureError(ctx, err).Send()
+
+		// Log error
+		t.Logger.WithError(err).Error("Failed to make request")
+		return events.APIGatewayProxyResponse{}, nil
 	}
 
-	log.Printf("Received event: %s", b)
-
-	t := toolbox.GetToolbox()
-	t.Logger.Info("Logger")
-	httpClient := t.GetHTTPClient(nil)
-	resp, err := httpClient.Get("https://google.com")
+	resp, err := httpClient.Do(req)
 	if err != nil {
+		// Capture error with tracer
+		apm.CaptureError(ctx, err).Send()
+
+		// Log error
 		t.Logger.WithError(err).Error("Failed to make request")
+		return events.APIGatewayProxyResponse{}, nil
 	}
 	t.Logger.WithField("StatusCode", resp.StatusCode).Info("Got response")
+	resp.Body.Close()
 
+	// Start a span
+	span := geoipTx.StartSpan("OpenDB", "job", nil)
+	// Open DB
 	db, err := geoip2.Open("GeoLite2-City.mmdb")
 	if err != nil {
-		log.Fatal(err)
+		apm.CaptureError(ctx, err).Send()
 		return events.APIGatewayProxyResponse{
 			StatusCode: http.StatusInternalServerError,
 			Body:       err.Error(),
 		}, err
 	}
 	defer db.Close()
+	span.End()
 
-	ipString := "72.210.63.111"
-
+	span = geoipTx.StartSpan("GetUserIP", "job", nil)
+	ipString := "72.210.63.111" // Default ip
 	ipParam, found := request.QueryStringParameters["ip"]
 	if found {
-		log.Printf("You specified: %s", ipParam)
+		t.Logger.WithField("IP", ipParam).Info("Got supplied IP")
 		ipString = ipParam
 	}
-
+	span.Context.SetLabel("IP", ipString)
 	ip := net.ParseIP(ipString)
+	span.End()
 
 	// If you are using strings that may be invalid, check that ip is not nil
+	span = geoipTx.StartSpan("ProcessUserIP", "job", nil)
 	record, err := db.City(ip)
 	if err != nil {
-		log.Fatal(err)
+		apm.CaptureError(ctx, err).Send()
 		return events.APIGatewayProxyResponse{
 			StatusCode: http.StatusInternalServerError,
 			Body:       err.Error(),
 		}, err
 	}
+	span.End()
 
-	fmt.Printf("English city name: %v\n", record.City.Names["en"])
-	if len(record.Subdivisions) > 0 {
-		fmt.Printf("English subdivision name: %v\n", record.Subdivisions[0].Names["en"])
-	}
-	fmt.Printf("English country name: %v\n", record.Country.Names["en"])
-
-	fmt.Printf("ISO country code: %v\n", record.Country.IsoCode)
-	fmt.Printf("Time zone: %v\n", record.Location.TimeZone)
-	fmt.Printf("Coordinates: %v, %v\n", record.Location.Latitude, record.Location.Longitude)
+	t.Logger.WithFields(logrus.Fields{
+		"EnglishCity":    record.City.Names["en"],
+		"EnglishCountry": record.Country.Names["en"],
+		"ISOCountryCode": record.Country.IsoCode,
+		"TimeZone":       record.Location.TimeZone,
+		"Lat":            record.Location.Latitude,
+		"Long":           record.Location.Longitude,
+	}).Info("Found result")
 
 	js, err := json.Marshal(record)
 	if err != nil {
-		log.Fatal(err)
+		apm.CaptureError(ctx, err).Send()
 		return events.APIGatewayProxyResponse{
 			StatusCode: http.StatusInternalServerError,
 			Body:       err.Error(),
--- go.mod
+++ go.mod
@@ -4,9 +4,10 @@ go 1.15
 
 require (
 	github.com/aws/aws-lambda-go v1.19.1
-	github.com/oschwald/geoip2-golang v1.4.0 // indirect
+	github.com/oschwald/geoip2-golang v1.4.0
 	github.secureserver.net/threat/util v0.0.0-20201017001357-49a72cdebd27
-	go.elastic.co/apm/module/apmlambda v1.8.0 // indirect
+	go.elastic.co/apm v1.8.0
+	go.elastic.co/apm/module/apmlambda v1.8.0
 )
 
 replace github.secureserver.net/threat/util => ../../threat/util
