ea955a7cf6fd09f20b00f3e263284b65de77428b
Greg Bailey <gbailey@godaddy.com>
2020-11-20 20:16:54 -0700

    Helper class to convert a DynamoDB numerical elements to JSON

--- apis/job/job.py
+++ apis/job/job.py
@@ -1,7 +1,20 @@
 """Lambda function to retrieve job status and output for ThreatTools API"""
 
+import decimal
 import json
 import boto3
+
+
+class DecimalEncoder(json.JSONEncoder):
+    """Helper class to convert a DynamoDB item to JSON"""
+
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if o % 1 > 0:
+                return float(o)
+            return int(o)
+        return super().default(o)
+
 
 # pylint: disable=unused-argument
 def handler(event, context):
@@ -14,7 +27,10 @@ def handler(event, context):
             .get_item(Key={"jobId": event["pathParameters"]["jobId"]})
         )
         if "Item" in results:
-            return {"statusCode": 200, "body": json.dumps(results["Item"])}
+            return {
+                "statusCode": 200,
+                "body": json.dumps(results["Item"], cls=DecimalEncoder),
+            }
 
         return {"statusCode": 404, "body": json.dumps("Not Found")}
 
