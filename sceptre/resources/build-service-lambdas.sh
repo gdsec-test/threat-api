69f94c87d7164f97b0d109b126bdc5da871e022b
Greg Bailey <gbailey@godaddy.com>
2020-12-21 21:13:10 -0700

    Add service lambdas to sceptre templates
    The lambdas found under the apis/ directory are created and updated by the
    sceptre templates.  Each subdirectory must contain a "lambda.json" file that
    describes the parameters used to create the lambda, and a "build.sh" script
    that's used to build the lambda.  The output of "build.sh" is expected to be a
    ZIP file "function.zip" which is uploaded to S3.  The SHA1 hash of the .go or
    .py source file is used as the S3 key for the build, so that sceptre and
    cloudformation can intelligently decide whether the function needs to be
    updated.
    
    The functions are currently built during each invocation, whether they've been
    updated or not.

---
+++ sceptre/resources/build-service-lambdas.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# This script supports the build process for the service lambdas:
+# - Store a SHA1 hash of the source code so the CloudFormation templates can
+#   use them to detect differences and trigger an update
+# - Build the lambdas
+# - Create ZIP files and upload them to S3 so that the CloudFormation templates
+#   can reference them
+
+set -eu
+
+THREAT_API_SOURCE=$(cd `dirname $0`/../.. && pwd)
+
+CODE_BUCKET=$(aws s3api list-buckets --output text --query 'Buckets[?ends_with(Name, `code-bucket`)].Name')
+RESOURCES_DIR=${THREAT_API_SOURCE}/sceptre/resources
+SERVICE_LAMBDAS=$(ls ${THREAT_API_SOURCE}/apis)
+
+for LAMBDA in ${SERVICE_LAMBDAS}
+do
+    echo Building ${LAMBDA} lambda
+
+    pushd ${THREAT_API_SOURCE}/apis/${LAMBDA}
+
+    if [ -x "build.sh" ]; then
+
+        # Store the SHA1 hash of the source code
+        if [ -f "${LAMBDA}.go" ]; then
+            SHA1HASH=$(sha1sum "${LAMBDA}.go" | cut -d' ' -f1)
+        elif [ -f "${LAMBDA}.py" ]; then
+            SHA1HASH=$(sha1sum "${LAMBDA}.py" | cut -d' ' -f1)
+        else
+            SHA1HASH=$(sha1sum "build.sh" | cut -d' ' -f1)
+        fi
+        echo ${SHA1HASH} > ${RESOURCES_DIR}/${LAMBDA}.sha1
+
+        # Build the lambda using the supplied build script
+        ./build.sh
+
+        # Upload the ZIP file to S3
+        aws s3 cp function.zip s3://${CODE_BUCKET}/${LAMBDA}/${SHA1HASH}
+
+        # Cleanup
+        rm -f ${LAMBDA} function.zip
+    fi
+
+    popd
+
+done
