633a767ee1191abe5bbcad1c5f654cd75a2eba04
Connor Lake <clake1@godaddy.com>
2021-01-06 14:50:51 -0800

    Added whois module

---
+++ apis/whois/triage.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/csv"
+	"encoding/json"
+	"fmt"
+
+	"github.com/gdcorp-infosec/threat-api/apis/triagec/triage"
+)
+
+// TriageModule triage module
+type TriageModule struct {
+}
+
+// GetDocs of this module
+func (m *TriageModule) GetDocs() *triage.Doc {
+	return &triage.Doc{Name: triageModuleName, Description: "Performs a whois lookup on domains"}
+}
+
+// Supports returns true of we support this ioc type
+func (m *TriageModule) Supports() []triage.IOCType {
+	return []triage.IOCType{triage.DomainType}
+}
+
+// Triage Finds whois information from a list of domains
+func (m *TriageModule) Triage(ctx context.Context, triageRequest *triage.Request) ([]*triage.Data, error) {
+	whoisResults, stats := Lookup(ctx, triageRequest.IOCs)
+
+	triageData := &triage.Data{
+		Title:    "Whois lookup data",
+		Metadata: []string{},
+	}
+
+	// Add some metadata if we found something interesting in the whois stats
+	validDomains := len(triageRequest.IOCs) - stats.InvalidDomains
+	if validDomains != len(stats.SameRegistrant) {
+		// Some domains have the same registrant
+		triageData.Metadata = append(triageData.Metadata, fmt.Sprintf("Out of %d valid domains, they are owned by %d unique registrants", validDomains, len(stats.SameRegistrant)))
+	}
+	if validDomains != len(stats.SameRegistrar) {
+		// Some domains have the same registrar
+		triageData.Metadata = append(triageData.Metadata, fmt.Sprintf("Out of %d valid domains, they are registered by %d unique registrars", validDomains, len(stats.SameRegistrar)))
+	}
+	if stats.InvalidDomains > 0 {
+		triageData.Metadata = append(triageData.Metadata, fmt.Sprintf("%d/%d Domains are invalid (bad whois data)", stats.InvalidDomains, len(triageRequest.IOCs)))
+	}
+
+	// Dump full data if we are doing full dump
+	if triageRequest.Verbose {
+		result, err := json.Marshal(whoisResults)
+		if err != nil {
+			triageData.Data = fmt.Sprintf("Error marshaling: %s", err)
+			return []*triage.Data{triageData}, nil
+		}
+		triageData.Data = string(result)
+		triageData.DataType = triage.JSONType
+		return []*triage.Data{triageData}, nil
+	}
+
+	// Dump data as simplified csv instead
+	resp := bytes.Buffer{}
+	csv := csv.NewWriter(&resp)
+	// Write headers
+	csv.Write([]string{
+		"domain",
+		"createdDate",
+		"updatedDate",
+		"expirationDate",
+		"registrarName",
+		"registrarEmail",
+		"registrarPhone",
+		"registrantName",
+		"registrantEmail",
+		"registrantPhone",
+		"registrantOrganization",
+		"registrantStreet",
+		"registrantCity",
+		"registrantCountry",
+		"administrativeOrganization",
+	})
+
+	// Write each domain whois info
+	for _, result := range whoisResults {
+		csv.Write([]string{
+			result.Domain.Domain,
+			result.Domain.CreatedDate,
+			result.Domain.UpdatedDate,
+			result.Domain.ExpirationDate,
+			result.Registrar.Name,
+			result.Registrar.Email,
+			result.Registrar.Phone,
+			result.Registrant.Name,
+			result.Registrant.Email,
+			result.Registrant.Phone,
+			result.Registrant.Organization,
+			result.Registrant.Street,
+			result.Registrant.City,
+			result.Registrant.Country,
+			result.Administrative.Organization,
+		})
+	}
+	csv.Flush()
+
+	triageData.Data = resp.String()
+	return []*triage.Data{triageData}, nil
+}
---
+++ apis/whois/whois.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/likexian/whois-go"
+	whoisparser "github.com/likexian/whois-parser-go"
+)
+
+const (
+	triageModuleName = "whois"
+)
+
+// WhoisStats contains some stats about the domains that have been looked up using whois
+type WhoisStats struct {
+	// The maps represent the name of the registrar or registrant paired to the whois results returned
+	SameRegistrar  map[string][]*whoisparser.WhoisInfo
+	SameRegistrant map[string][]*whoisparser.WhoisInfo
+	InvalidDomains int
+}
+
+// Lookup performs a whoislookup on the passed in domains
+func Lookup(ctx context.Context, domains []string) ([]*whoisparser.WhoisInfo, *WhoisStats) {
+	stats := &WhoisStats{
+		SameRegistrant: map[string][]*whoisparser.WhoisInfo{},
+		SameRegistrar:  map[string][]*whoisparser.WhoisInfo{},
+	}
+	// Helper function to add an entry to our stats map
+	addToMap := func(m map[string][]*whoisparser.WhoisInfo, key string, entry *whoisparser.WhoisInfo) {
+		if _, ok := m[key]; ok {
+			m[key] = append(m[key], entry)
+			return
+		}
+		m[key] = []*whoisparser.WhoisInfo{entry}
+	}
+
+	// Get WhoisInfo
+	whoisResults := []*whoisparser.WhoisInfo{}
+	for _, domain := range domains {
+		// Check context
+		select {
+		case <-ctx.Done():
+			break
+		default:
+		}
+
+		// AddErrRow is a standard to add an errored whois query to the list of results
+		addErrRow := func(err error) {
+			stats.InvalidDomains++
+			whoisResults = append(whoisResults, &whoisparser.WhoisInfo{Domain: &whoisparser.Domain{Domain: domain}, Registrant: &whoisparser.Contact{}, Registrar: &whoisparser.Contact{Name: fmt.Sprintf("ERROR: %s", err)}, Administrative: &whoisparser.Contact{}})
+		}
+
+		// Convert to the base domain
+		domainSplit := strings.Split(domain, ".")
+		if len(domainSplit) < 2 {
+			addErrRow(fmt.Errorf("not a domain"))
+			continue
+		}
+		domain = fmt.Sprintf("%s.%s", domainSplit[len(domainSplit)-2], domainSplit[len(domainSplit)-1])
+
+		// Look up domain
+		// TODO: fix log
+		// triage.Log(triageModuleName, "WhoisLookup", api, core.LogFields{"domain": domain})
+		whoisRaw, err := whois.Whois(domain)
+		if err != nil {
+			addErrRow(err)
+			continue
+		}
+		whoisResult, err := whoisparser.Parse(whoisRaw)
+		if err != nil {
+			addErrRow(err)
+			continue
+		}
+
+		// Fill in with blank data if an element is missing
+		if whoisResult.Registrant == nil {
+			whoisResult.Registrant = &whoisparser.Contact{}
+		}
+		if whoisResult.Registrar == nil {
+			whoisResult.Registrar = &whoisparser.Contact{}
+		}
+		if whoisResult.Administrative == nil {
+			whoisResult.Administrative = &whoisparser.Contact{}
+		}
+		if whoisResult.Domain == nil {
+			whoisResult.Domain = &whoisparser.Domain{}
+		}
+
+		// Add to stats and results
+		addToMap(stats.SameRegistrant, whoisResult.Registrant.Name, &whoisResult)
+		addToMap(stats.SameRegistrar, whoisResult.Registrar.Name, &whoisResult)
+		whoisResults = append(whoisResults, &whoisResult)
+
+		// Sleep a small time to avoid spamming their servers
+		// TODO: Add token bucket for 1000 queries per day
+		select {
+		case <-ctx.Done():
+			break
+		case <-time.After(time.Millisecond * 500):
+		}
+	}
+
+	return whoisResults, stats
+}
