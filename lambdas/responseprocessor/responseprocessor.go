584a775370250031264abfc5e8fe1ab36055d8b9
Connor Lake <clake1@godaddy.com>
2021-01-08 13:01:01 -0800

    Added support for getting module name from lambda ARN

--- lambdas/responseprocessor/responseprocessor.go
+++ lambdas/responseprocessor/responseprocessor.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"regexp"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -12,11 +13,16 @@ import (
 	"github.com/gdcorp-infosec/threat-api/lambdas/common"
 	"github.com/opentracing/opentracing-go"
 	"github.com/sirupsen/logrus"
+	"github.com/vertoforce/regexgrouphelp"
 	"github.secureserver.net/threat/util/lambda/toolbox"
 	_ "go.elastic.co/apm/module/apmlambda"
 )
 
 var t *toolbox.Toolbox
+
+var (
+	lambdaNameRegex = regexp.MustCompile(`\w+:\w+:\w+:.*?:.*?:.*?:(?P<lambdaName>.*?):`)
+)
 
 // Lambda function to take a completed job data and insert it in to the database
 func handler(ctx context.Context, request events.SQSEvent) (string, error) {
@@ -34,6 +40,12 @@ func handler(ctx context.Context, request events.SQSEvent) (string, error) {
 				"body":  string(sqsRecord.Body),
 			}).Error("Error unmarshaling completed job data")
 			continue
+		}
+		if completedLambdaData.ResponsePayload.ModuleName == "" {
+			// Get module name from ARN
+			if groups, ok := regexgrouphelp.FindRegexGroups(lambdaNameRegex, sqsRecord.EventSourceARN)["lambdaName"]; ok && len(groups) > 0 {
+				completedLambdaData.ResponsePayload.ModuleName = groups[0]
+			}
 		}
 		t.Logger.WithFields(logrus.Fields{"moduleName": completedLambdaData.ResponsePayload.ModuleName, "requestBody": sqsRecord.Body}).Info("Processing module response")
 
