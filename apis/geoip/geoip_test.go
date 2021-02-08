5519661ff0d73fd8671544684bf6a0f4ce514a88
Prashanthi Kanniappan Murthy <pkanniappanmurt@godaddy.com>
2021-02-08 11:59:36 -0800

    Tested tracing

--- apis/geoip/geoip_test.go
+++ apis/geoip/geoip_test.go
@@ -2,37 +2,38 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"github.com/gdcorp-infosec/threat-api/lambdas/common/triagelegacyconnector/triage"
 	"github.com/gdcorp-infosec/threat-util/lambda/toolbox"
-	"os"
+	"strings"
 	"testing"
 )
 
 func TestLookup(t *testing.T) {
 
-	fmt.Println(os.Getenv("ELASTIC_APM_SERVICE_NAME"))
 	ctx := context.Background()
 	toolbox := toolbox.GetToolbox()
 	defer toolbox.Close(ctx)
 
+	testIP := "72.210.63.111"
 	triageRequest := &triage.Request{
-		IOCs:     []string{"72.210.63.111"},
+		IOCs:     []string{testIP},
 		IOCsType: triage.IPType,
 	}
 	triageModule := TriageModule{}
 	triageResult, err := triageModule.Triage(ctx, triageRequest)
+	if err != nil {
+		t.Fatal(err)
+	}
 	toolbox.Close(ctx)
 
 	if len(triageResult) == 0 {
 		t.Fatal("len 0")
 	}
-
 	if triageResult[0].Data == "" {
 		t.Fatal("first data element empty ")
 	}
-	// TODO: assert triageResult
-	if err != nil {
-		t.Fatal(err)
+	if !strings.Contains(triageResult[0].Data, testIP) || !strings.Contains(triageResult[0].Data, "Phoenix") {
+		t.Fatal("module didnt return expected test data")
 	}
+
 }
