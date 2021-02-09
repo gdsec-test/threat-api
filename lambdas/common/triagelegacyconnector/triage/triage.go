f37cc3988ede1b6f5e74f5220617e0a90e2b6db2
Connor Lake <clake1@godaddy.com>
2021-02-09 13:37:21 -0800

    Changed to capital IOC types

--- lambdas/common/triagelegacyconnector/triage/triage.go
+++ lambdas/common/triagelegacyconnector/triage/triage.go
@@ -37,19 +37,19 @@ type IOCType string
 
 // IOCTypes
 const (
-	UnknownType IOCType = "unknown"
-	DomainType  IOCType = "domain"
-	EmailType   IOCType = "email"
-	CVEType     IOCType = "cve"
-	CWEType     IOCType = "cwe"
-	CAPECType   IOCType = "capec"
-	CPEType     IOCType = "cpe"
-	URLType     IOCType = "url"
-	MD5Type     IOCType = "md5"
-	SHA1Type    IOCType = "sha1"
-	SHA256Type  IOCType = "sha256"
-	SHA512Type  IOCType = "sha512"
-	IPType      IOCType = "ip"
+	UnknownType IOCType = "UNKNOWN"
+	DomainType  IOCType = "DOMAIN"
+	EmailType   IOCType = "EMAIL"
+	CVEType     IOCType = "CVE"
+	CWEType     IOCType = "CWE"
+	CAPECType   IOCType = "CAPEC"
+	CPEType     IOCType = "CPE"
+	URLType     IOCType = "URL"
+	MD5Type     IOCType = "MD5"
+	SHA1Type    IOCType = "SHA1"
+	SHA256Type  IOCType = "SHA256"
+	SHA512Type  IOCType = "SHA512"
+	IPType      IOCType = "IP"
 	// This is for godaddy machine hostnames
 	HostnameType IOCType = "hostname"
 	// AWS hostname
