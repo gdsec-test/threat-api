4e0ed052dc310346e719313e1b1a5f93550165f3
Connor Lake <clake1@godaddy.com>
2021-02-10 11:17:21 -0800

    Removed pascal case entries

--- lambdas/common/job.go
+++ lambdas/common/job.go
@@ -34,9 +34,9 @@ type JobDBEntry struct {
 	Responses  map[string]appencryption.DataRowRecord `dynamodbav:"responses" json:"-"`
 	Submission appencryption.DataRowRecord            `dynamodbav:"submission" json:"-"`
 	// Epoch start time
-	StartTime float64 `dynamodbav:"startTime" json:"StartTime"`
+	StartTime float64 `dynamodbav:"startTime" json:"startTime"`
 	// Count of total modules that should be run from this submission
-	TotalModules int `dynamodbav:"totalModules" json:"TotalModules"`
+	TotalModules int `dynamodbav:"totalModules" json:"totalModules"`
 
 	// Decrypted data
 	// The ignore tags in dynamodbav are to prevent the json tags
