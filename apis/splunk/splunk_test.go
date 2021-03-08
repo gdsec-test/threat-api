b7a62265f1cace98ef3bf28ee25ab0f0bd04fb87
Connor Lake <clake1@godaddy.com>
2021-03-08 14:33:47 -0800

    Added small splunk AWS test

--- apis/splunk/splunk_test.go
+++ apis/splunk/splunk_test.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"os"
 	"testing"
+
+	"github.com/gdcorp-infosec/threat-api/lambdas/common/toolbox"
+	"github.com/gdcorp-infosec/threat-api/lambdas/common/triagelegacyconnector/triage"
 )
 
 func TestSplunk(t *testing.T) {
@@ -26,5 +29,25 @@ func TestSplunk(t *testing.T) {
 	}
 	for result := range results {
 		fmt.Println(result)
+	}
+}
+
+// TestSplunkAWS is used to test splunk functionality including AWS functionality.
+// It does require some manual testing.
+func TestSplunkAWS(t *testing.T) {
+	tb = toolbox.GetToolbox()
+	defer tb.Close(context.Background())
+
+	tm := &TriageModule{}
+	triageData, err := tm.Triage(context.Background(), &triage.Request{
+		IOCs:     []string{"1.1.1.1"},
+		IOCsType: triage.IPType,
+		JWT:      os.Getenv("TESTING_JWT"),
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(triageData) == 0 {
+		t.Fatalf("no data returned")
 	}
 }
