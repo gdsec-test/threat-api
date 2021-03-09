a842195669501f91462d70090018e466e2d26d18
Connor Lake <clake1@godaddy.com>
2021-03-09 09:22:23 -0800

    Organized IP searching functions

--- apis/splunk/ip.go
+++ apis/splunk/ip.go
@@ -29,6 +29,9 @@ const (
 	maxConcurnetSplunkSearches = 5
 )
 
+// ipSearchFunction is a simple function that accepts an ip, and appends items to the triage metadata.
+type ipSearchFunction func(ip string)
+
 // triageIPs Finds failed login attempts and other events mentioning the ips.
 // It does not return any real data, instead linking to splunk searches
 func (m *TriageModule) triageIPs(ctx context.Context, triageRequest *triage.Request) []*triage.Data {
@@ -40,131 +43,115 @@ func (m *TriageModule) triageIPs(ctx context.Context, triageRequest *triage.Requ
 	metadataLock := sync.Mutex{}                              // Lock access to the metadata slice
 	threadLimit := make(chan int, maxConcurnetSplunkSearches) // Use a buffered channel to limit the amount of threads we spawn
 
-	// Start a multithreaded operation performing splunk searches (with a max thread limit)
-	// And getting the results
+	// Here we define all of our different search functions for IPs.  Each function needs to search splunk,
+	// then perform some intelligence logic to create a `triage.Data` entry with insights (metadata) that it found.
+	searchOnPremFailedLoginEvents := ipSearchFunction(func(ip string) {
+		defer func() {
+			<-threadLimit
+			wg.Done()
+		}()
+		results, search, err := m.performSplunkSearch(ctx, fmt.Sprintf(onPremLoginEvent, ip))
+		if err != nil {
+			return
+		}
+		for result := range results {
+			if result["count"] == "0" {
+				continue
+			}
+			metadataLock.Lock()
+			triageData.Metadata = append(triageData.Metadata, fmt.Sprintf("*%s* had >=*%s* <%s|failed on prem logins> to *%s* in the last *%d* days", ip, result["count"], search.URL(), result["product"], recentLoginsBackcheckDays))
+			metadataLock.Unlock()
+		}
+	})
+	searchGenericIPEvents := ipSearchFunction(func(ip string) {
+		defer func() {
+			<-threadLimit
+			wg.Done()
+		}()
+		results, search, err := m.performSplunkSearch(ctx, fmt.Sprintf(eventWithIP, ip))
+		if err != nil {
+			return
+		}
+		for result := range results {
+			if result["count"] == "0" {
+				continue
+			}
+			metadataLock.Lock()
+			triageData.Metadata = append(triageData.Metadata, fmt.Sprintf("*%s* was found in >=*%s* <%s|events in splunk> in the last *%d* days", ip, result["count"], search.URL(), recentLoginsBackcheckDays))
+			metadataLock.Unlock()
+		}
+	})
+	searchOktaLogins := ipSearchFunction(func(ip string) {
+		defer func() {
+			<-threadLimit
+			wg.Done()
+		}()
+		results, search, err := m.performSplunkSearch(ctx, fmt.Sprintf(recentOktaLoginsByIPSearch, ip))
+		if err != nil {
+			return
+		}
+		userCount := 0
+		loginCount := uint64(0)
+		for result := range results {
+			userCount++
+			userLoginCount, _ := strconv.ParseUint(result.GetFieldString("count"), 10, 64)
+			loginCount += userLoginCount
+		}
+		if userCount > 0 || loginCount > 0 {
+			metadataLock.Lock()
+			triageData.Metadata = append(triageData.Metadata,
+				fmt.Sprintf("*%s* had >=*%d* <%s|okta logins> from >=*%d* unique users in the last *%d* days",
+					ip,
+					loginCount,
+					search.URL(),
+					userCount,
+					recentLoginsBackcheckDays,
+				))
+			metadataLock.Unlock()
+		}
+	})
+	searchAWSItems := ipSearchFunction(func(ip string) {
+		defer func() {
+			<-threadLimit
+			wg.Done()
+		}()
+		results, search, err := m.performSplunkSearch(ctx, fmt.Sprintf(awsConfigItemSearch, ip, ip))
+		if err != nil {
+			return
+		}
+		for result := range results {
+			metadataLock.Lock()
+			triageData.Metadata = append(triageData.Metadata, fmt.Sprintf(
+				"*%s* was found <%s|associated with an AWS item>.  AWS Account ID: *%s* Host: *%s* Product: *%s* Private IP: *%s* Public IP: *%s* AWS Region: *%s*",
+				ip,
+				search.URL(),
+				result["awsAccountId"],
+				result["host"],
+				result["product"],
+				result["configuration.privateIpAddress"],
+				result["configuration.association.publicIp"],
+				result["awsRegion"],
+			))
+			metadataLock.Unlock()
+		}
+	})
+
+	ipSearchFunctions := []ipSearchFunction{searchOnPremFailedLoginEvents, searchGenericIPEvents, searchOktaLogins, searchAWSItems}
+
+	// Start a multithreaded operation performing splunk searches from our function list (with a max thread limit)
+	// And getting the results.
 ipLoop:
 	for _, ip := range triageRequest.IOCs {
-		// Search for on prem failed login events
-		select {
-		case <-ctx.Done():
-			break ipLoop
-		case threadLimit <- 1:
-			wg.Add(1)
+		for _, ipSearchFunction := range ipSearchFunctions {
+			select {
+			case <-ctx.Done():
+				break ipLoop
+			case threadLimit <- 1:
+				wg.Add(1)
+			}
+			// Search for on prem failed login events
+			go ipSearchFunction(ip)
 		}
-		go func(ip string) {
-			defer func() {
-				<-threadLimit
-				wg.Done()
-			}()
-			results, search, err := m.performSplunkSearch(ctx, fmt.Sprintf(onPremLoginEvent, ip))
-			if err != nil {
-				return
-			}
-			for result := range results {
-				if result["count"] == "0" {
-					continue
-				}
-				metadataLock.Lock()
-				triageData.Metadata = append(triageData.Metadata, fmt.Sprintf("*%s* had >=*%s* <%s|failed on prem logins> to *%s* in the last *%d* days", ip, result["count"], search.URL(), result["product"], recentLoginsBackcheckDays))
-				metadataLock.Unlock()
-			}
-		}(ip)
-
-		// Search for any generic IP events
-		select {
-		case <-ctx.Done():
-			break ipLoop
-		case threadLimit <- 1:
-			wg.Add(1)
-		}
-		go func(ip string) {
-			defer func() {
-				<-threadLimit
-				wg.Done()
-			}()
-			results, search, err := m.performSplunkSearch(ctx, fmt.Sprintf(eventWithIP, ip))
-			if err != nil {
-				return
-			}
-			for result := range results {
-				if result["count"] == "0" {
-					continue
-				}
-				metadataLock.Lock()
-				triageData.Metadata = append(triageData.Metadata, fmt.Sprintf("*%s* was found in >=*%s* <%s|events in splunk> in the last *%d* days", ip, result["count"], search.URL(), recentLoginsBackcheckDays))
-				metadataLock.Unlock()
-			}
-		}(ip)
-
-		// Search for okta logins from IP
-		select {
-		case <-ctx.Done():
-			break ipLoop
-		case threadLimit <- 1:
-			wg.Add(1)
-		}
-		go func(ip string) {
-			defer func() {
-				<-threadLimit
-				wg.Done()
-			}()
-			results, search, err := m.performSplunkSearch(ctx, fmt.Sprintf(recentOktaLoginsByIPSearch, ip))
-			if err != nil {
-				return
-			}
-			userCount := 0
-			loginCount := uint64(0)
-			for result := range results {
-				userCount++
-				userLoginCount, _ := strconv.ParseUint(result.GetFieldString("count"), 10, 64)
-				loginCount += userLoginCount
-			}
-			if userCount > 0 || loginCount > 0 {
-				metadataLock.Lock()
-				triageData.Metadata = append(triageData.Metadata,
-					fmt.Sprintf("*%s* had >=*%d* <%s|okta logins> from >=*%d* unique users in the last *%d* days",
-						ip,
-						loginCount,
-						search.URL(),
-						userCount,
-						recentLoginsBackcheckDays,
-					))
-				metadataLock.Unlock()
-			}
-		}(ip)
-
-		// Search for AWS item
-		select {
-		case <-ctx.Done():
-			break ipLoop
-		case threadLimit <- 1:
-			wg.Add(1)
-		}
-		go func(ip string) {
-			defer func() {
-				<-threadLimit
-				wg.Done()
-			}()
-			results, search, err := m.performSplunkSearch(ctx, fmt.Sprintf(awsConfigItemSearch, ip, ip))
-			if err != nil {
-				return
-			}
-			for result := range results {
-				metadataLock.Lock()
-				triageData.Metadata = append(triageData.Metadata, fmt.Sprintf(
-					"*%s* was found <%s|associated with an AWS item>.  AWS Account ID: *%s* Host: *%s* Product: *%s* Private IP: *%s* Public IP: *%s* AWS Region: *%s*",
-					ip,
-					search.URL(),
-					result["awsAccountId"],
-					result["host"],
-					result["product"],
-					result["configuration.privateIpAddress"],
-					result["configuration.association.publicIp"],
-					result["awsRegion"],
-				))
-				metadataLock.Unlock()
-			}
-		}(ip)
 	}
 	wg.Wait()
 
