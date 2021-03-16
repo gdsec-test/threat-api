38354704103dc59241d604e4ed11aa2fc5f5750b
Connor Lake <clake1@godaddy.com>
2021-03-15 17:30:37 -0700

    Removed sleep in test

--- lambdas/common/toolbox/tracing_test.go
+++ lambdas/common/toolbox/tracing_test.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"testing"
-	"time"
 )
 
 func TestTracing(t *testing.T) {
@@ -30,6 +29,4 @@ func TestTracing(t *testing.T) {
 
 	// Now manually go check APM to see if the correct structure appeared.
 	// Also check appsec logging to see if the logs were sent correctly.
-
-	time.Sleep(time.Second * 3)
 }
