c9e06b5f04dec266e0916d99e87dc35df79d5648
Connor Lake <clake1@godaddy.com>
2021-03-19 14:13:22 -0700

    Added secrets retriveal and metadata

--- apis/cmap/entry.go
+++ apis/cmap/entry.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -10,14 +12,45 @@ import (
 	"github.com/gdcorp-infosec/threat-api/lambdas/common/triagelegacyconnector"
 )
 
+const (
+	cmapCredentialsStoreKey = toolbox.SecretsMangerSecretIDIntegrationsPrefix + "cmap"
+)
+
 var tb *toolbox.Toolbox
 
 func handler(ctx context.Context, request events.SNSEvent) ([]*common.CompletedJobData, error) {
 	tb := toolbox.GetToolbox()
 	defer tb.Close(ctx)
 
-	geoIPTriageModule := TriageModule{}
-	return triagelegacyconnector.AWSToTriage(ctx, tb, &geoIPTriageModule, request)
+	cmapModule, err := initCMAPModule(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("error creating module: %w", err)
+	}
+	return triagelegacyconnector.AWSToTriage(ctx, tb, cmapModule, request)
+}
+
+func initCMAPModule(ctx context.Context) (*TriageModule, error) {
+	// Get CMAP cert and key from secret store
+	secret, err := tb.GetFromCredentialsStore(ctx, cmapCredentialsStoreKey, nil)
+	if err != nil {
+		return nil, fmt.Errorf("error getting cmap secret: %w", err)
+	}
+
+	// Unmarshal
+	cmapCreds := struct {
+		CMAPCert string `json:"cmap_cert"`
+		CMAPKey  string `json:"cmap_key"`
+	}{}
+	err = json.Unmarshal([]byte(*secret.SecretString), &cmapCreds)
+	if err != nil {
+		return nil, fmt.Errorf("error unmarshalling cmap creds: %w", err)
+	}
+	geoIPTriageModule := &TriageModule{
+		CMAPCert: cmapCreds.CMAPCert,
+		CMAPKey:  cmapCreds.CMAPKey,
+	}
+
+	return geoIPTriageModule, nil
 }
 
 func main() {
