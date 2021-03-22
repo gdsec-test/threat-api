fb87cfe375e22ce051857a79f843cef958f832dc
Courtney Falk <cfalk@godaddy.com>
2021-03-22 16:12:49 -0400

    Initial commit of the VirusTotal directory.

---
+++ apis/virustotal/triage.go
@@ -0,0 +1,198 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/csv"
+	"fmt"
+
+	"github.com/gdcorp-infosec/threat-api/lambdas/common/toolbox"
+	"github.com/gdcorp-infosec/threat-api/lambdas/common/triagelegacyconnector/triage"
+)
+
+const (
+	paramUrlhausAsns = "URLhaus-ASNs"
+)
+
+// Triage module
+type TriageModule struct {
+}
+
+// GetDocs of this module
+func (m *TriageModule) GetDocs() *triage.Doc {
+	return &triage.Doc{Name: triageModuleName, Description: "Find hosted malware from urlhaus based on hash, domain, or URL."}
+}
+
+// Supports returns true of we support this IoC type
+func (m *TriageModule) Supports() []triage.IOCType {
+	return []triage.IOCType{
+		triage.DomainType,
+		triage.IPType,
+		triage.URLType,
+		triage.MD5Type,
+		triage.SHA256Type,
+	}
+}
+
+func (m *TriageModule) GetAsns(ctx context.Context) (string, error) {
+	t := toolbox.GetToolbox()
+	defer t.Close(ctx)
+
+	parameter, err := t.GetFromParameterStore(context.Background(), paramUrlhausAsns, false)
+	if err != nil {
+		return "", err
+	}
+	return *parameter.Value, nil
+}
+
+// Triage finds malware domains according to URLhaus by ASN
+func (m *TriageModule) Triage(ctx context.Context, triageRequest *triage.Request) ([]*triage.Data, error) {
+	triageData := &triage.Data{
+		Title:    "URLhaus",
+		Metadata: []string{},
+	}
+
+	switch triageRequest.IOCsType {
+	case triage.MD5Type:
+		triageData.Title = "Malicious URLs hosting this MD5 hash (URLhaus)"
+		entries := make([]*UrlhausPayloadEntry, len(triageRequest.IOCs))
+		for i, ioc := range triageRequest.IOCs {
+			entry, err := GetMd5(ctx, ioc)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			entries[i] = entry
+		}
+		triageData.Data = HashesToCsv(entries)
+	case triage.SHA256Type:
+		triageData.Title = "Malicious URLs hosting this SHA256 hash (URLhaus)"
+		entries := make([]*UrlhausPayloadEntry, len(triageRequest.IOCs))
+		for i, ioc := range triageRequest.IOCs {
+			entry, err := GetSha256(ctx, ioc)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			entries[i] = entry
+		}
+		triageData.Data = HashesToCsv(entries)
+	case triage.DomainType, triage.IPType:
+		triageData.Title = "Information about this host (URLhaus)"
+		entries := make([]*UrlhausHostEntry, len(triageRequest.IOCs))
+		for i, ioc := range triageRequest.IOCs {
+			entry, err := GetDomainOrIp(ctx, ioc)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			entries[i] = entry
+		}
+		triageData.Data = HostsToCsv(entries)
+	case triage.URLType:
+		triageData.Title = "Information about this URL address (URLhaus)"
+		entries := make([]*UrlhausUrlEntry, len(triageRequest.IOCs))
+		for i, ioc := range triageRequest.IOCs {
+			entry, err := GetUrl(ctx, ioc)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			entries[i] = entry
+		}
+		triageData.Data = UrlsToCsv(entries)
+	}
+
+	return []*triage.Data{triageData}, nil
+}
+
+func HostsToCsv(hosts []*UrlhausHostEntry) string {
+	// Dump data into CSV format
+	resp := bytes.Buffer{}
+	csv := csv.NewWriter(&resp)
+	// Headers
+	csv.Write([]string{
+		"First Seen",
+		"URL Summary",
+		"Spamhaus",
+		"SURBL",
+	})
+	// Rows
+	for _, host := range hosts {
+		if host == nil {
+			continue
+		}
+		cols := []string{
+			host.First,
+			fmt.Sprintf("Seen at %d different URLs", host.Count),
+			host.Blacklists.SpamhausStatus,
+			host.Blacklists.SurblStatus,
+		}
+		csv.Write(cols)
+	}
+	csv.Flush()
+
+	return resp.String()
+}
+
+func UrlsToCsv(urls []*UrlhausUrlEntry) string {
+	// Dump data into CSV format
+	resp := bytes.Buffer{}
+	csv := csv.NewWriter(&resp)
+	// Headers
+	csv.Write([]string{
+		"Host",
+		"Status",
+		"Added",
+		"Taken Down",
+	})
+	// Rows
+	for _, url := range urls {
+		if url == nil {
+			continue
+		}
+		cols := []string{
+			url.Host,
+			url.Status,
+			url.Added,
+			fmt.Sprint(url.Takedown),
+		}
+		csv.Write(cols)
+	}
+	csv.Flush()
+
+	return resp.String()
+}
+
+func HashesToCsv(payloads []*UrlhausPayloadEntry) string {
+	// Dump data into CSV format
+	resp := bytes.Buffer{}
+	csv := csv.NewWriter(&resp)
+	// Headers
+	csv.Write([]string{
+		"MD5",
+		"SHA256",
+		"File Type",
+		"File Size",
+		"First Seen",
+		"URL Summary",
+	})
+	// Rows
+	for _, payload := range payloads {
+		if payload == nil {
+			continue
+		}
+		cols := []string{
+			payload.Md5,
+			payload.Sha,
+			payload.FileType,
+			fmt.Sprint(payload.Size),
+			payload.First,
+			fmt.Sprintf("Seen at %d different URLs", payload.UrlCount),
+		}
+		csv.Write(cols)
+	}
+	csv.Flush()
+
+	return resp.String()
+}
---
+++ apis/virustotal/virustotal.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"context"
+	"encoding/csv"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"golang.org/x/net/context/ctxhttp"
+)
+
+const (
+	triageModuleName = "virustotal"
+	baseUrl          = "https://www.virustotal.com/api/v3/"
+	apiHashUrl       = baseUrl + "files/"
+)
+
+func QueryApi(ctx context.Context, apiUrl string, key string, value string) ([]byte, error) {
+	//resp, err := http.PostForm(apiUrl, url.Values{key: {value}})
+	resp, err := ctxhttp.PostForm(ctx, http.DefaultClient, apiUrl, url.Values{key: {value}})
+	if err != nil {
+		fmt.Printf("Error in POST: %s", err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("Error in reading response body: %s", err)
+		return nil, err
+	}
+	return body, nil
+}
+
+func DownloadAsns(ctx context.Context, asns []string) []*urlHausEntry {
+	entries := []*urlHausEntry{}
+
+	for _, asn := range asns {
+		data, err := FetchSingleAsn(asn)
+		if err != nil {
+			continue
+		}
+
+		// URLhaus returns API queries as a CSV file
+		csvReader := csv.NewReader(strings.NewReader(data))
+		csvReader.Comment = '#'
+		records, err := csvReader.ReadAll()
+
+		for _, field := range records {
+			// filter out the offline domains
+			if field[2] == "online" {
+				entry := &urlHausEntry{
+					Date:      field[0],
+					URL:       field[1],
+					URLStatus: field[2],
+					Threat:    field[3],
+					Tags:      field[4],
+					Host:      field[5],
+					IPAddress: field[6],
+					ASnumber:  field[7],
+					Country:   field[8],
+				}
+				entries = append(entries, entry)
+			}
+		}
+	}
+
+	return entries
+}
+
+func GetMd5(ctx context.Context, md5 string) (*UrlhausPayloadEntry, error) {
+	body, err := QueryApi(ctx, apiHashUrl, "md5_hash", md5)
+	if err != nil {
+		return nil, err
+	}
+	if len(body) == 0 {
+		return nil, errors.New("No error reported but the body was empty")
+	}
+	var entry UrlhausPayloadEntry
+	err = json.Unmarshal(body, &entry)
+	if err != nil {
+		return nil, err
+	}
+	if entry.Status != "ok" {
+		return nil, fmt.Errorf("Query for %s returned no results (%s)", md5, entry.Status)
+	}
+	return &entry, nil
+}
+
+func GetSha256(ctx context.Context, sha256 string) (*UrlhausPayloadEntry, error) {
+	body, err := QueryApi(ctx, apiHashUrl, "sha256_hash", sha256)
+	if err != nil {
+		return nil, err
+	}
+	if len(body) == 0 {
+		return nil, errors.New("No error reported but the body was empty")
+	}
+	var entry UrlhausPayloadEntry
+	err = json.Unmarshal(body, &entry)
+	if err != nil {
+		return nil, err
+	}
+	if entry.Status != "ok" {
+		return nil, errors.New(fmt.Sprintf("Query for %s returned no results (%s)", sha256, entry.Status))
+	}
+	return &entry, nil
+}
+
+func GetDomainOrIp(ctx context.Context, host string) (*UrlhausHostEntry, error) {
+	body, err := QueryApi(ctx, apiHostUrl, "host", host)
+	if err != nil {
+		return nil, err
+	}
+	if len(body) == 0 {
+		return nil, errors.New("No error reported but the body was empty")
+	}
+	var entry UrlhausHostEntry
+	err = json.Unmarshal(body, &entry)
+	if err != nil {
+		return nil, err
+	}
+	if entry.Status != "ok" {
+		return nil, errors.New(fmt.Sprintf("Query for %s returned no results (%s)", host, entry.Status))
+	}
+	return &entry, nil
+}
+
+func GetUrl(ctx context.Context, _url string) (*UrlhausUrlEntry, error) {
+	body, err := QueryApi(ctx, apiUrlUrl, "url", _url)
+	if err != nil {
+		return nil, err
+	}
+	if len(body) == 0 {
+		return nil, errors.New("No error reported but the body was empty")
+	}
+	var entry UrlhausUrlEntry
+	err = json.Unmarshal(body, &entry)
+	if err != nil {
+		return nil, err
+	}
+	if entry.Status != "ok" {
+		return nil, errors.New(fmt.Sprintf("Query for %s returned no results (%s)", _url, entry.Status))
+	}
+	return &entry, nil
+}
