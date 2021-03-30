b1d268df76abf92ffbd1c8c0e635ccc59c5aea05
Connor Lake <clake1@godaddy.com>
2021-03-30 15:04:59 -0700

    Simpler way to load PEM block.  Working except that you need to set GODEBUG=x509ignoreCN=0

--- apis/splunk/splunk.go
+++ apis/splunk/splunk.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"crypto/tls"
+	"crypto/x509"
 	"net/http"
 
 	"github.com/vertoforce/go-splunk"
@@ -15,42 +16,15 @@ func (m *TriageModule) initClient(ctx context.Context) error {
 	}
 
 	// Parse the splunk certificate chain PEM block
-	splunkCertChain := tls.Certificate{}
-	var certDERBlock *pem.Block
-	certPEMBlock := []byte(splunkDefaultCertChainRaw)
-	for {
-		certDERBlock, certPEMBlock = pem.Decode(certPEMBlock)
-		if certDERBlock == nil {
-			break
-		}
-		if certDERBlock.Type == "CERTIFICATE" {
-			splunkCertChain.Certificate = append(splunkCertChain.Certificate, certDERBlock.Bytes)
-		}
-	}
-	// Build TLS config, adding x509 certificates
 	tlsConfig := &tls.Config{}
 	tlsConfig.RootCAs = x509.NewCertPool()
-	for _, cert := range splunkCertChain.Certificate {
-		x509Cert, err := x509.ParseCertificate(cert)
-		if err != nil {
-			return fmt.Errorf("bad splunk certificate")
-		}
-		x509Cert.Subject.Names = append(x509Cert.Subject.Names, pkix.AttributeTypeAndValue{Type: asn1.ObjectIdentifier{2, 5, 4, 3}, Value: "godaddy.splunkcloud.com"})
-		x509Cert.Subject.CommonName = ""
-		fmt.Printf("%s", x509Cert.Subject.Names)
-		tlsConfig.RootCAs.AddCert(x509Cert)
-	}
-	tlsConfig.ServerName = "SplunkServerDefaultCert"
 	tlsConfig.RootCAs.AppendCertsFromPEM([]byte(splunkDefaultCertChainRaw))
-	tlsConfig.BuildNameToCertificate()
+	tlsConfig.ServerName = "SplunkServerDefaultCert"
 
 	client, err := splunk.NewClient(ctx, m.SplunkUsername, m.SplunkPassword, &splunk.Config{
 		HTTPClient: &http.Client{
 			Transport: &http.Transport{
-				TLSClientConfig: &tls.Config{
-					// TODO: Change this to accept the cert instead
-					InsecureSkipVerify: true,
-				},
+				TLSClientConfig: tlsConfig,
 			},
 		},
 		BaseURL: m.SplunkBaseURL,
