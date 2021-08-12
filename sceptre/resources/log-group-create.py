9873b8cc198afb18397e8d8f4b2008f08e6d94d4
Prashanthi Kanniappan Murthy <pkanniappanmurt@godaddy.com>
2021-08-12 07:29:32 -0700

    log group sceptre

---
+++ sceptre/resources/log-group-create.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+import os
+import boto3
+
+from pathlib2 import Path
+
+PATH = os.path.dirname(os.path.abspath(__file__))
+ROOT_THREAT_API = str(Path(PATH).parents[1])
+PATH_API = os.path.join(ROOT_THREAT_API, "apis")
+
+
+def create_log_group(log_group_name):
+    logs_client = boto3.client("logs")
+    try:
+        response = logs_client.create_log_group(
+            logGroupName=log_group_name,
+        )
+
+    except Exception as exception:
+        print("group already exists")
+    else:
+        print(
+            "create_log_group() response: %d"
+            % response["ResponseMetadata"]["HTTPStatusCode"]
+        )
+
+        return response["ResponseMetadata"]["HTTPStatusCode"]
+
+
+def put_retention_policy(log_group_name, retention_policy):
+    logs_client = boto3.client("logs")
+    try:
+        response = logs_client.put_retention_policy(
+            logGroupName=log_group_name, retentionInDays=retention_policy
+        )
+    except Exception as exception:
+        print("exception in retention policy")
+    else:
+        print(
+            "put_retention_policy() response: %d"
+            % response["ResponseMetadata"]["HTTPStatusCode"]
+        )
+
+        return response["ResponseMetadata"]["HTTPStatusCode"]
+
+
+def create_log_groups_all_apis():
+    for subdir, _, _ in os.walk(PATH_API):
+        if (subdir.split("/")[-1] == "apis") or ("Library" in subdir):
+            continue
+        log_group_name = "/aws/lambda/" + subdir.split("/")[-1]
+        print("------> " + log_group_name)
+        response_code = create_log_group(log_group_name)
+        if response_code != 200:
+            continue
+        response_code = put_retention_policy(log_group_name, 30)
+        if response_code != 200:
+            continue
+
+
+if __name__ == "__main__":
+    create_log_groups_all_apis()
