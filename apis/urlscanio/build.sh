51dd2215bf5ae85bda6fa2b329f38602c7e3b64e
ogomozov <ogomozov@godaddy.com>
2021-12-03 15:42:41 -0800

    fix generation lambda scripts

--- apis/urlscanio/build.sh
+++ apis/urlscanio/build.sh
@@ -3,4 +3,4 @@
 set -eu
 env GOPRIVATE=github.secureserver.net,github.com/gdcorp-* GOOS=linux GOARCH=amd64 go build -o urlscanio
 rm -f function.zip
-zip -9 function.zip urlscanio
+zip -9q function.zip urlscanio
