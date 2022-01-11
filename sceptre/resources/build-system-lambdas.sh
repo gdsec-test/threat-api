cdaf62023fef239121bcb9a49575495564c86882
ogomozov <ogomozov@godaddy.com>
2022-01-11 14:13:11 -0800

    Add Vulnerability watch function to read CVEs and write them in DynamoDB

--- sceptre/resources/build-system-lambdas.sh
+++ sceptre/resources/build-system-lambdas.sh
@@ -14,7 +14,7 @@ THREAT_API_SOURCE=$(cd `dirname $0`/../.. && pwd)
 
 CODE_BUCKET=$(aws s3api list-buckets --output text --query 'Buckets[?ends_with(Name, `code-bucket`)].Name')
 RESOURCES_DIR=${THREAT_API_SOURCE}/sceptre/resources
-SYSTEM_LAMBDAS="manager responseprocessor"
+SYSTEM_LAMBDAS="manager responseprocessor vulnerabilitywatch"
 
 for LAMBDA in ${SYSTEM_LAMBDAS}
 do
