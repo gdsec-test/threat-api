3284d671ce3551e343d59d11dc776c79351b576f
ogomozov <ogomozov@godaddy.com>
2022-05-13 11:41:37 -0700

    revert broken auth

--- sceptre/resources/authorizer/build.sh
+++ sceptre/resources/authorizer/build.sh
@@ -26,7 +26,7 @@ if [ "${RC}" != "0" ]; then
 
     aws lambda create-function \
         --function-name authorizer \
-        --runtime python3.7 \
+        --runtime python3.6 \
         --role ${ROLE_ARN} \
         --region us-west-2 \
         --timeout 3 \
